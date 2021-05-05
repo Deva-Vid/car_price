133fabedc9a9686d77ecff9436e0b6959159ea1e
Deva-Vid <earthaxis66@gmail.com>
2021-05-05 18:14:42 +0530

    car_price name

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import numpy as np
 import sklearn
 from sklearn.   preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('E:\\python practice\\car _kaggle\\radom_forest_regression_model.pkl', 'rb'))
+model = pickle.load(open('E:\\python practice\\car_kaggle\\radom_forest_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
