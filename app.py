4ada1cb44243ccf96c63a6a64fa747c3c0d67558
Deva-Vid <earthaxis66@gmail.com>
2021-05-05 18:45:06 +0530

    updated

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import requests
 import pickle
 import numpy as np
 import sklearn
-from sklearn.   preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
-model = pickle.load(open('E:\\python practice\\car_kaggle\\radom_forest_regression_model.pkl', 'rb'))
+model = pickle.load(open('random_forest_regression_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
@@ -26,12 +26,9 @@ def predict():
         if(Fuel_Type_Petrol=='Petrol'):
                 Fuel_Type_Petrol=1
                 Fuel_Type_Diesel=0
-        elif(Fuel_Type_Petrol == 'Diesel'):
+        else:
             Fuel_Type_Petrol=0
             Fuel_Type_Diesel=1
-        else:
-            Fuel_Type_Petrol = 0
-            Fuel_Type_Petrol = 1
         Year=2020-Year
         Seller_Type_Individual=request.form['Seller_Type_Individual']
         if(Seller_Type_Individual=='Individual'):
